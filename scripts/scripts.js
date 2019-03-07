dc7461c64d6a22cb3c50b8f1356dd3e8ce5b9ffb
Eric Wittenberg <ebwittenberg@gmail.com>
2019-03-07 15:59:42 -0500

    appends one character name to the list

---
+++ scripts/scripts.js
@@ -0,0 +1,12 @@
+
+
+let namesDiv = document.querySelector('[data-names-list]');
+
+const oneChar = characters[0].name;
+
+const para1 = document.createElement('p');
+para1.textContent = oneChar;
+namesDiv.append(para1);
+
+
+
